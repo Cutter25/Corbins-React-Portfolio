9fa91b90b43a25e4da10bdb2163eb793adb8559d
Corbin Cutter <corbinc03123@gmail.com>
2023-02-08 00:45:40 -0500

    Ready to build tomorrow :)

--- corbins-react-portfolio/src/App.js
+++ corbins-react-portfolio/src/App.js
@@ -2,14 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 // Importing components
+import Header from './components/Header';
+
+
+
+// Practice components lol
 import Greet from './components/Greet'
 import Welcome from './components/Welcome'
+// Practice components lol
 
 function App() {
   return (
     <div className="App">
-      <Greet name="Corbin"/>
-      <Welcome />
+      <Header />
     </div>
   );
 }
--- corbins-react-portfolio/src/components/Header.js
+++ corbins-react-portfolio/src/components/Header.js
@@ -1,10 +1,21 @@
 import React, { useState } from 'react';
 
+function Header() {
+    const [header, setHeader] = useState([]);
 
-// Header
+    return (
+        <div className='header'>
+            <div className='header-welcome'>
+                {/* Welcome message goes here */}
+            </div>
 
+            <div className='header-nav'>
+                {/* navbar goes here */}
+            </div>
 
-
+        </div>
+    );
+};
 
 
 export default Header;
