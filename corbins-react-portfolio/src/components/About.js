327de178f9e78a41a8d457d7c9c5128369c0f35b
Corbin Cutter <corbinc03123@gmail.com>
2023-02-13 14:57:10 -0500

    Working on portfolio page still

--- corbins-react-portfolio/src/components/About.js
+++ corbins-react-portfolio/src/components/About.js
@@ -4,7 +4,7 @@ function About() {
     return (
         <div>
             <main>
-                <img className="about-me-pic" src={process.env.PUBLIC_URL + '/images/IMG_2468.jpg'} alt="Headshot photograph of Corbin Cutter"/>
+                <img className="about-me-pic" src={process.env.PUBLIC_URL + "/images/IMG_2468.jpg"} alt="Headshot photograph of Corbin Cutter"/>
                 
                 <p className="about-me-p">Hello user! My name is Corbin Cutter. I am an aspiring full-stack web developer/ software engineer.
                 I am proficient in HTML, CSS, Javascript, SQL, React, and I plan to master many more computer languages.</p>
--- corbins-react-portfolio/src/components/Work.js
+++ corbins-react-portfolio/src/components/Work.js
@@ -5,28 +5,45 @@ function Work() {
         <div>
             <main>
                 <div className="project-container">
-
-
+                    <img className="project-img" src={process.env.PUBLIC_URL + "/images/NoteTakerScreenshot.png"} alt="Corbin's note taker web application" />
+                    <a className="project-github" href="https://github.com/Cutter25/Corbins-Note-Taker"> <img className="github-logo" 
+                    src={process.env.PUBLIC_URL + "/images/Github2.png"} alt="Github Logo" />  </a>
+                    <a className="project-live-site" href="https://corbins-note-taker.herokuapp.com/"><p> Note Taker </p></a>
                 </div>
 
                 <div className="project-container">
-
+                    <img className="project-img" src={process.env.PUBLIC_URL + "/images/NoteTakerScreenshot.png"} alt="Corbin's note taker web application" />
+                    <a className="project-github" href="https://github.com/Cutter25/Corbins-Note-Taker"> <img className="github-logo" 
+                    src={process.env.PUBLIC_URL + "/images/Github2.png"} alt="Github Logo" />  </a>
+                    <a className="project-live-site" href="https://corbins-note-taker.herokuapp.com/"><p> Note Taker </p></a>
                 </div>
 
                 <div className="project-container">
-
+                    <img className="project-img" src={process.env.PUBLIC_URL + "/images/NoteTakerScreenshot.png"} alt="Corbin's note taker web application" />
+                    <a className="project-github" href="https://github.com/Cutter25/Corbins-Note-Taker"> <img className="github-logo" 
+                    src={process.env.PUBLIC_URL + "/images/Github2.png"} alt="Github Logo" />  </a>
+                    <a className="project-live-site" href="https://corbins-note-taker.herokuapp.com/"><p> Note Taker </p></a>
                 </div>
 
                 <div className="project-container">
-
+                    <img className="project-img" src={process.env.PUBLIC_URL + "/images/NoteTakerScreenshot.png"} alt="Corbin's note taker web application" />
+                    <a className="project-github" href="https://github.com/Cutter25/Corbins-Note-Taker"> <img className="github-logo" 
+                    src={process.env.PUBLIC_URL + "/images/Github2.png"} alt="Github Logo" />  </a>
+                    <a className="project-live-site" href="https://corbins-note-taker.herokuapp.com/"><p> Note Taker </p></a>
                 </div> 
 
                 <div className="project-container">
-
+                    <img className="project-img" src={process.env.PUBLIC_URL + "/images/NoteTakerScreenshot.png"} alt="Corbin's note taker web application" />
+                    <a className="project-github" href="https://github.com/Cutter25/Corbins-Note-Taker"> <img className="github-logo" 
+                    src={process.env.PUBLIC_URL + "/images/Github2.png"} alt="Github Logo" />  </a>
+                    <a className="project-live-site" href="https://corbins-note-taker.herokuapp.com/"><p> Note Taker </p></a>
                 </div>
 
                 <div className="project-container">
-
+                    <img className="project-img" src={process.env.PUBLIC_URL + "/images/NoteTakerScreenshot.png"} alt="Corbin's note taker web application" />
+                    <a className="project-github" href="https://github.com/Cutter25/Corbins-Note-Taker"> <img className="github-logo" 
+                    src={process.env.PUBLIC_URL + "/images/Github2.png"} alt="Github Logo" />  </a>
+                    <a className="project-live-site" href="https://corbins-note-taker.herokuapp.com/"><p> Note Taker </p></a>
                 </div>
             </main>
         </div>
