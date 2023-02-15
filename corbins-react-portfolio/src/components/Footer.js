08d3854ab939ab9edbd30d4b7dc101f89761dfd1
Corbin Cutter <corbinc03123@gmail.com>
2023-02-15 14:55:42 -0500

    Finally done

--- corbins-react-portfolio/src/components/Footer.js
+++ corbins-react-portfolio/src/components/Footer.js
@@ -4,7 +4,10 @@ function Footer() {
     return (
         <div>
             <main className='footer'>
-                <p>Footer</p>
+                <p className='footer-p'>Profile Links</p>
+                <a className='footer-github' href={process.env.PUBLIC_URL + 'https://github.com/Cutter25'}>GitHub</a>
+                <a className='footer-linkedin' href={process.env.PUBLIC_URL + 'https://www.linkedin.com/in/corbincutter/'}>LinkedIn</a>
+                <a className='footer-stack-overflow' href={process.env.PUBLIC_URL + 'https://stackoverflow.com/users/21222084/corbin-cutter'}>stack overflow</a>
             </main>
         </div>
     );
--- corbins-react-portfolio/src/components/Resume.js
+++ corbins-react-portfolio/src/components/Resume.js
@@ -4,7 +4,8 @@ function Resume() {
     return (
         <div>
             <main>
-                <p>Resume link!</p>
+                <a className="resume-link" href={process.env.PUBLIC_URL + "https://docs.google.com/document/d/1jc8ukTFXlIsUHldLoWm-ilB4u0HCB3gSfUIwGmCsagw/edit?usp=sharing"} target="_blank" rel="noreferrer"
+                >Click here to view my Resume!</a>
             </main>
         </div>
     );
